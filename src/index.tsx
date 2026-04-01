77f0cde1699a8e9ede3e13ed1667ca1e869e47c3
abdulwahab <abdulwahabmahmoud99@gmail.com>
2026-04-01 15:47:28 +0200

    Refactor: Remove jsconfig.json and several theme-related files, update package.json and package-lock.json to include craco and TypeScript dependencies, and adjust start, build, and test scripts for craco integration.

---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "i18n";
+import App from "App";
+import { MaterialUIControllerProvider } from "context";
+import { AuthProvider } from "shared/context/AuthContext";
+import { CartProvider } from "shared/context/CartContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const container = document.getElementById("app");
+if (!container) {
+  throw new Error("Root element #app not found");
+}
+const root = createRoot(container);
+
+export const mainQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 120,
+      gcTime: 1000 * 60 * 10,
+      retry: 1,
+      refetchOnWindowFocus: false,
+      throwOnError: true,
+    },
+  },
+});
+
+root.render(
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
+      <QueryClientProvider client={mainQueryClient}>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </MaterialUIControllerProvider>
+  </BrowserRouter>,
+);
